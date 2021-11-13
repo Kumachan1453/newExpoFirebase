6abd601bf7ef7165b41cbd06624c2f03905c4811
阪本周平 <github000only@gmail.com>
2021-11-13 11:10:54 +0900

    Expo Firebase環境構築（Firebaseインストール済）

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBXRAvJhNWr4xFOIRvEqFKA82gO6v8LZnU",
+    authDomain: "newexpofirebase-cda46.firebaseapp.com",
+    projectId: "newexpofirebase-cda46",
+    storageBucket: "newexpofirebase-cda46.appspot.com",
+    messagingSenderId: "772345587846",
+    appId: "1:772345587846:web:211794f811cadbe05bc907",
+  };
+
+  const initial = initializeApp(firebaseConfig);
+  const db = getFirestore();
+
+  const snapshot = db.collection("users").get();
+  // const snapshot = db;　だとうまくいく。
+  console.log("snapshot", snapshot);
+  // snapshot.forEach((doc) => {
+  //   console.log(doc.id, "=>", doc.data());
+  // });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
