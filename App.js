b300b2995e42f52e6bf7686fb955971e10b58a20
阪本周平 <github000only@gmail.com>
2021-11-20 07:25:18 +0900

    今の状態だとエラーが発生します。しかし、書き換えたので、更新します。

--- App.js
+++ App.js
@@ -14,15 +14,59 @@ export default function App() {
     appId: "1:772345587846:web:211794f811cadbe05bc907",
   };
 
-  const initial = initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
   const db = getFirestore();
+  // // const snapshot = db.collection("users").get();
+  // const snapshot = db;
+  // // 「const snapshot = db;」だとうまくいく。
+  // console.log("snapshot", snapshot);
 
-  const snapshot = db.collection("users").get();
-  // const snapshot = db;　だとうまくいく。
-  console.log("snapshot", snapshot);
-  // snapshot.forEach((doc) => {
-  //   console.log(doc.id, "=>", doc.data());
-  // });
+  const citiesRef = db.collection("cities");
+  // const citiesRef = db;
+
+  citiesRef.doc("SF").set({
+    name: "San Francisco",
+    state: "CA",
+    country: "USA",
+    capital: false,
+    population: 860000,
+  });
+  citiesRef.doc("LA").set({
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA",
+    capital: false,
+    population: 3900000,
+  });
+  citiesRef.doc("DC").set({
+    name: "Washington, D.C.",
+    state: null,
+    country: "USA",
+    capital: true,
+    population: 680000,
+  });
+  citiesRef.doc("TOK").set({
+    name: "Tokyo",
+    state: null,
+    country: "Japan",
+    capital: true,
+    population: 9000000,
+  });
+  citiesRef.doc("BJ").set({
+    name: "Beijing",
+    state: null,
+    country: "China",
+    capital: true,
+    population: 21500000,
+  });
+
+  const cityRef = db.collection("cities").doc("SF");
+  const doc = cityRef.get();
+  if (!doc.exists) {
+    console.log("No such document!");
+  } else {
+    console.log("Document data:", doc.data());
+  }
 
   return (
     <View style={styles.container}>
